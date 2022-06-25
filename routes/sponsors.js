1acbc1d81b323c7dd5e2bc45c311de1148dd8992
Jae Wu <jaewuchun@gmail.com>
2022-06-25 15:33:48 -0700

    Sponsor detail screen

--- routes/sponsors.js
+++ routes/sponsors.js
@@ -21,4 +21,31 @@ router.get('/', verify, async (req, res) => {
     }
 });
 
+
+// Get one sponsor (sponsor detail screen)
+router.get('/:id', verify, getSponsor, async (req, res) => {
+    res.json(res.sponsor);
+})
+
+async function getSponsor(req, res, next) {
+    let sponsor;
+
+    try {
+        sponsor = await Sponsor.findById(req.params.id);
+        if (sponsor == null) {
+            return res.status(404).json({
+                message: `Cannot find sponsor with id ${req.params.id}`
+            });
+        }
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+
+    res.sponsor = sponsor;
+    next();
+}
+
+
 module.exports = router;
