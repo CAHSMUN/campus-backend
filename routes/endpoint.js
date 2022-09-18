94f6b5b5852d8e1733e81cb7c7284ce2c33deb0e
Jae Wu <jaewuchun@gmail.com>
2022-09-18 13:06:12 -0700

    added logging for listener

--- routes/endpoint.js
+++ routes/endpoint.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    
+    console.log(req)
+    res.status(200)
 })
 
 module.exports = router;
